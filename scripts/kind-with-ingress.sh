7ae807c335191c1f40e026b07c1e0d09e4005b2c
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-08-24 16:29:46 -0400

    fixed dashboard secret/token creation
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- scripts/kind-with-ingress.sh
+++ scripts/kind-with-ingress.sh
@@ -30,12 +30,15 @@ echo ${CLUSTER_CA} | base64 -d > cluster.crt
 # https://www.containiq.com/post/intro-to-kubernetes-dashboards
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
 kubectl apply -f ./k8s/dashboard-admin.yaml
+# kubectl describe serviceaccount admin-user -n kubernetes-dashboard
+# kubectl describe secret admin-user-token -n kubernetes-dashboard
 
-echo "Dashboard admin token: "
 # dashboard_admin_token=$(kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode)
-export dashboard_admin_token=$(kubectl -n kubernetes-dashboard create token admin-user)
+# export dashboard_admin_token=$(kubectl -n kubernetes-dashboard create token admin-user)
+export dashboard_admin_token=$(kubectl get secret -n kubernetes-dashboard admin-user-token -o jsonpath="{.data.token}" | base64 --decode)
 echo "${dashboard_admin_token}" > dashboard-admin-token.txt
 kubectl config set-credentials cluster-admin --token=${dashboard_admin_token}
+echo "Dashboard Token: ${dashboard_admin_token}"
 
 # https://github.com/kubernetes/ingress-nginx
 echo "deploy nginx ingress for kind"
