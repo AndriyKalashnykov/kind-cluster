a2742eaabf62e7858df4f15e1754c1957b4eecd4
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2022-09-02 11:51:57 -0400

    fix: updated workflow
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- scripts/kind-with-registry.sh
+++ scripts/kind-with-registry.sh
@@ -16,7 +16,7 @@ fi
 cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
-containerdConfigPatches:
+containerdConfigPatches: 
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
     endpoint = ["http://${reg_name}:5000"]
