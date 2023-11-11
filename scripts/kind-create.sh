090f6666e5e724a311468a0f11cacd7981385e8b
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-11-10 22:32:52 -0500

    fix: updated kind-create
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- scripts/kind-create.sh
+++ scripts/kind-create.sh
@@ -8,7 +8,14 @@ cd $SCRIPT_PARENT_DIR
 # https://kind.sigs.k8s.io/
 # https://cloudyuga.guru/hands_on_lab/kind-k8s
 
+if [ $(kind get clusters | grep ^kind$) ]
+then
+    echo "Kind cluster Cluster exists skipping creation ..."
+    echo "Switching context to kind ..."
+    kubectl config use-context kind-kind
+else
 kind create cluster --config=./k8s/kind-config.yaml --name kind --wait 10s
 docker container ls --format "table {{.Image}}\t{{.State}}\t{{.Names}}"
+fi
 
 cd $LAUNCH_DIR
